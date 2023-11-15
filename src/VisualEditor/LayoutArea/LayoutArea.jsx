493d1c40dfaf3fdc7c1831daeccaba298d2412b6
Daisy Wang <daisywang@Daisys-MacBook-Air.local>
2023-11-14 16:47:38 -0800

    added horizontal split feature

--- src/VisualEditor/LayoutArea/LayoutArea.jsx
+++ src/VisualEditor/LayoutArea/LayoutArea.jsx
@@ -3,63 +3,255 @@ import { Droppable, Draggable } from 'react-beautiful-dnd'
 import { Card, Button } from '@mantine/core'
 import { IconSquareRoundedPlus } from '@tabler/icons-react'
 
-export default function LayoutArea({ droppedPictures, onAddSquare }) {
-  const imageSize = 700 / droppedPictures.length
+export default function LayoutArea({
+  droppedPictures,
+  onAddSquare,
+  verticalSquareCellCount,
+  handleSplitCell,
+}) {
+  const handleSplit = (index, isHorizontal) => () => {
+    handleSplitCell(index, isHorizontal)
+  }
+
+  const imageSize = 800 / verticalSquareCellCount
   const [hoveredIndex, setHoveredIndex] = React.useState(null)
 
-  return (
-    <>
-      {droppedPictures.map((square, index) => (
-        <Card
-          key={square.id}
-          withBorder
-          radius='sm'
-          style={{
-            marginTop: '5px',
-            height: `250px`,
-            padding: 0,
-            border: '2px dotted lightgrey',
-            background: index === hoveredIndex ? '#e6f7ff' : 'transparent',
-            boxShadow:
-              square.picture && index === hoveredIndex
-                ? '0 4px 8px rgba(0, 0, 0, 0.1)'
-                : 'none',
-          }}
-          onMouseEnter={() => setHoveredIndex(index)}
-          onMouseLeave={() => setHoveredIndex(null)}
-        >
-          <Droppable key={square.id} droppableId={square.id} type='group'>
-            {(boxProvided) => (
-              <div {...boxProvided.droppableProps} ref={boxProvided.innerRef}>
-                {square.picture && (
-                  <Draggable
-                    draggableId={square.picture.id}
-                    key={square.picture.id}
-                    index={index}
-                  >
-                    {(dragProvided) => (
-                      <div
-                        {...dragProvided.draggableProps}
-                        {...dragProvided.dragHandleProps}
-                        ref={dragProvided.innerRef}
-                      >
-                        <Card radius='sm' style={{ margin: '5px', padding: 0 }}>
-                          <img
-                            src={square.picture.picture}
-                            style={{ width: '100%', height: `${imageSize}px` }}
-                          />
-                        </Card>
-                      </div>
-                    )}
-                  </Draggable>
-                )}
-                {boxProvided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </Card>
-      ))}
+  const resultComponents = [];
+  let totalWidth = 0;
+  let outerGroup = [];
+  let group = [];
+  let currentGroupWidth = 0;
+  let horizontalDirection = true;
 
+  for(let i = 0; i < droppedPictures.length; ){
+    
+    const currentPicture = droppedPictures[i];
+
+    if (!currentPicture.isSplit || currentPicture.horizontalSplitCount === 0) {
+      const heightCut = Math.pow(2, currentPicture.verticalSplitCount)
+
+        console.log(i);
+        resultComponents.push(
+                <Card
+                    key={currentPicture.id}
+                    withBorder
+                    radius='sm'
+                    style={{
+                      marginTop: '5px',
+                      marginRight: '5px',
+                      height: `${imageSize/heightCut}px`,
+                      width: `100%`,
+                      padding: 0,
+                      border: '2px dotted lightgrey',
+                      background: i === hoveredIndex ? '#e6f7ff' : 'transparent',
+                      boxShadow:
+                      currentPicture.picture && i === hoveredIndex
+                          ? '0 4px 8px rgba(0, 0, 0, 0.1)'
+                          : 'none',
+                    }}
+                    // onMouseEnter={() => setHoveredIndex(i)}
+                    // onMouseLeave={() => setHoveredIndex(null)}
+                  >
+                    <Droppable key={currentPicture.id} droppableId={currentPicture.id} type='group'>
+                      {(boxProvided) => (
+                        <div {...boxProvided.droppableProps} ref={boxProvided.innerRef}>
+                          {currentPicture.picture && (
+                            <Draggable
+                              draggableId={currentPicture.picture.id}
+                              key={currentPicture.picture.id}
+                              index={i}
+                            >
+                              {(dragProvided) => (
+                                <div
+                                  {...dragProvided.draggableProps}
+                                  {...dragProvided.dragHandleProps}
+                                  ref={dragProvided.innerRef}
+                                >
+                                  <Card
+                                    radius='sm'
+                                    style={{ margin: '5px', padding: 0 }}
+                                  >
+                                    <img
+                                      src={currentPicture.picture.picture}
+                                      style={{
+                                        width: '100%',
+                                        height: `${(imageSize / heightCut)-10}px`,
+                                      }}
+                                    />
+                                  </Card>
+                                </div>
+                              )}
+                            </Draggable>
+                          )}
+                          {boxProvided.placeholder}
+                        </div>
+                      )}
+                    </Droppable>
+                    {currentPicture.picture && (
+                      <>
+                        <Button
+                          variant='light'
+                          size='sm'
+                          radius='sm'
+                          onClick={handleSplit(i, true)}
+                          style={{
+                            position: 'absolute',
+                            bottom: '5px',
+                            right: '5px',
+                            marginBottom: '30px',
+                          }}
+                        >
+                          SplitH
+                        </Button>
+                        <Button
+                          variant='light'
+                          size='sm'
+                          radius='sm'
+                          onClick={handleSplit(i, false)}
+                          style={{
+                            position: 'absolute',
+                            bottom: '5px',
+                            right: '5px',
+                          }}
+                        >
+                          SplitV
+                        </Button>
+                      </>
+                    )}
+                  </Card>
+        );
+        i++;
+    } else {
+
+      while (i < droppedPictures.length && droppedPictures[i].id.length === currentPicture.id.length) {
+        const nextPicture = droppedPictures[i];
+        horizontalDirection = nextPicture.isHorizontalSplit;
+        const heightCut = Math.pow(2, nextPicture.verticalSplitCount)
+        const widthCut = Math.pow(2, nextPicture.horizontalSplitCount);
+        console.log("widthcut" + widthCut);
+        console.log("heightcut" +heightCut)
+ 
+        group.push(
+                <Card
+                            key={nextPicture.id}
+                            withBorder
+                            radius='sm'
+                            style={{
+                              marginTop: '5px',
+                              marginRight: '5px',
+                              height: `${imageSize/heightCut}px`,
+                              width: `${nextPicture.widthPercent}%`,
+                              padding: 0,
+                              border: '2px dotted lightgrey',
+                              background: i === hoveredIndex ? '#e6f7ff' : 'transparent',
+                              boxShadow:
+                              nextPicture.picture && i === hoveredIndex
+                                  ? '0 4px 8px rgba(0, 0, 0, 0.1)'
+                                  : 'none',
+                            }}
+                            // onMouseEnter={() => setHoveredIndex(i)}
+                            // onMouseLeave={() => setHoveredIndex(null)}
+                          >
+                            <Droppable key={nextPicture.id} droppableId={nextPicture.id} type='group'>
+                              {(boxProvided) => (
+                                <div {...boxProvided.droppableProps} ref={boxProvided.innerRef}>
+                                  {nextPicture.picture && (
+                                    <Draggable
+                                      draggableId={nextPicture.picture.id}
+                                      key={nextPicture.picture.id}
+                                      index={i}
+                                    >
+                                      {(dragProvided) => (
+                                        <div
+                                          {...dragProvided.draggableProps}
+                                          {...dragProvided.dragHandleProps}
+                                          ref={dragProvided.innerRef}
+                                        >
+                                          <Card
+                                            radius='sm'
+                                            style={{ margin: '5px', padding: 0 }}
+                                          >
+                                            <img
+                                              src={nextPicture.picture.picture}
+                                              style={{
+                                                width: '100%',
+                                                height: `${(imageSize / heightCut)}px`,
+                                              }}
+                                            />
+                                          </Card>
+                                        </div>
+                                      )}
+                                    </Draggable>
+                                  )}
+                                  {boxProvided.placeholder}
+                                </div>
+                              )}
+                            </Droppable>
+                            {nextPicture.picture && (
+                              <>
+                                <Button
+                                  variant='light'
+                                  size='sm'
+                                  radius='sm'
+                                  onClick={handleSplit(i, true)}
+                                  style={{
+                                    position: 'absolute',
+                                    bottom: '5px',
+                                    right: '5px',
+                                    marginBottom: '30px',
+                                  }}
+                                >
+                                  {i}
+                                </Button>
+                                <Button
+                              
+                                  variant='light'
+                                  size='sm'
+                                  radius='sm'
+                                  onClick={handleSplit(i, false)}
+                                  style={{
+                                    position: 'absolute',
+                                    bottom: '5px',
+                                    right: '5px',
+                                  }}
+                                >
+                                  {i}
+                                </Button>
+                              </>
+                            )}
+                          </Card>
+        );
+        totalWidth += 100 / Math.pow(2, nextPicture.horizontalSplitCount);
+        currentGroupWidth += 100 / Math.pow(2, nextPicture.horizontalSplitCount);
+        i++;
+      }
+
+      outerGroup.push(
+        <div key={currentPicture.id} style={{ display: 'flex', flexDirection: horizontalDirection ? "row" : "column", width: `${currentGroupWidth}%`}}>
+          {group}
+        </div>
+      )
+      group = [];
+      currentGroupWidth = 0;
+
+      if(totalWidth === 100){
+        resultComponents.push(
+          <div key={currentPicture.id} style={{ display: 'flex', flexDirection:"row", width: "100%"}}>
+            {outerGroup}
+          </div>
+        );
+        totalWidth = 0;
+        outerGroup = [];
+      }
+    } 
+  }
+  console.log(resultComponents);
+  
+  return (
+    <div>
+      {resultComponents.map((component, index) => (
+        <React.Fragment key={index}>{component}</React.Fragment>
+      ))}
       <Button
         variant='light'
         size='sm'
@@ -74,7 +266,129 @@ export default function LayoutArea({ droppedPictures, onAddSquare }) {
         }
       >
         Add New Cell
-      </Button>
-    </>
-  )
+      </Button> 
+    </div>
+  );
+
+
+//   const [prevGroupDir, setPrevGroupDir] = React.useState(true)
+
+  //for each droppedPicture, we get its width and height first
+  // return (
+  //   <>
+  //     {droppedPictures.map((square, index) => {
+  //       const widthCut = Math.pow(2, square.horizontalSplitCount);
+  //       const heightCut = Math.pow(2, square.verticalSplitCount)
+  //       console.log(square.isHorizontalSplit)
+  //       // setCurrentOccupiedWidth(currentOccupiedWidth+(100/widthCut));
+
+  //       return (
+  //           <Card
+  //                   key={square.id}
+  //                   withBorder
+  //                   radius='sm'
+  //                   style={{
+  //                     marginTop: '5px',
+  //                     marginRight: '5px',
+  //                     height: `${square.isSplit ? (imageSize / heightCut) : "250px"}`,
+  //                     width: `${100/widthCut}%`,
+  //                     padding: 0,
+  //                     border: '2px dotted lightgrey',
+  //                     background: index === hoveredIndex ? '#e6f7ff' : 'transparent',
+  //                     boxShadow:
+  //                       square.picture && index === hoveredIndex
+  //                         ? '0 4px 8px rgba(0, 0, 0, 0.1)'
+  //                         : 'none',
+  //                   }}
+  //                   onMouseEnter={() => setHoveredIndex(index)}
+  //                   onMouseLeave={() => setHoveredIndex(null)}
+  //                 >
+  //                   <Droppable key={square.id} droppableId={square.id} type='group'>
+  //                     {(boxProvided) => (
+  //                       <div {...boxProvided.droppableProps} ref={boxProvided.innerRef}>
+  //                         {square.picture && (
+  //                           <Draggable
+  //                             draggableId={square.picture.id}
+  //                             key={square.picture.id}
+  //                             index={index}
+  //                           >
+  //                             {(dragProvided) => (
+  //                               <div
+  //                                 {...dragProvided.draggableProps}
+  //                                 {...dragProvided.dragHandleProps}
+  //                                 ref={dragProvided.innerRef}
+  //                               >
+  //                                 <Card
+  //                                   radius='sm'
+  //                                   style={{ margin: '5px', padding: 0 }}
+  //                                 >
+  //                                   <img
+  //                                     src={square.picture.picture}
+  //                                     style={{
+  //                                       width: '100%',
+  //                                       height: `${(imageSize / heightCut)}px`,
+  //                                     }}
+  //                                   />
+  //                                 </Card>
+  //                               </div>
+  //                             )}
+  //                           </Draggable>
+  //                         )}
+  //                         {boxProvided.placeholder}
+  //                       </div>
+  //                     )}
+  //                   </Droppable>
+  //                   {square.picture && (
+  //                     <>
+  //                       <Button
+  //                         variant='light'
+  //                         size='sm'
+  //                         radius='sm'
+  //                         onClick={() => handleSplit(index, true)}
+  //                         style={{
+  //                           position: 'absolute',
+  //                           bottom: '5px',
+  //                           right: '5px',
+  //                           marginBottom: '30px',
+  //                         }}
+  //                       >
+  //                         SplitH
+  //                       </Button>
+  //                       <Button
+  //                         variant='light'
+  //                         size='sm'
+  //                         radius='sm'
+  //                         onClick={() => handleSplit(index, false)}
+  //                         style={{
+  //                           position: 'absolute',
+  //                           bottom: '5px',
+  //                           right: '5px',
+  //                         }}
+  //                       >
+  //                         SplitV
+  //                       </Button>
+  //                     </>
+  //                   )}
+  //                 </Card>
+  //           )
+  //       })} 
+    
+
+  //             <Button
+  //       variant='light'
+  //       size='sm'
+  //       radius='sm'
+  //       onClick={onAddSquare}
+  //       style={{ marginTop: '10px' }}
+  //       rightSection={
+  //         <IconSquareRoundedPlus
+  //           stroke={1.25}
+  //           style={{ width: '1.15rem', height: '1.25rem' }}
+  //         />
+  //       }
+  //     >
+  //       Add New Cell
+  //     </Button> 
+  //   </>
+  // )
 }
--- src/VisualEditor/VisualEditor.jsx
+++ src/VisualEditor/VisualEditor.jsx
@@ -4,15 +4,22 @@ import LayoutArea from './LayoutArea/LayoutArea'
 import { Grid, Container, Card } from '@mantine/core'
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 import PICTURES from './PicturesData'
-import bg from './bg.svg'
 
 export default function VisualEditor() {
   //array of object, each object contains a id and a picture, and picture has two fields: id and picture
-  const initialDestinationSquares = Array.from({ length: 4 }, (_, index) => ({
-    id: `square-${index + 1}`,
-    picture: null,
-  }))
+  const initialDestinationSquares = 
+    Array.from({ length: 4 }, (_, index) => ({
+      id: `square-${index + 1}`,
+      picture: null,
+      isSplit: false,
+      isHorizontalSplit: false,
+      horizontalSplitCount: 0,
+      verticalSplitCount: 0,
+      widthPercent: 100,
+    }))
 
+  const [verticalSquareCellCount, setVerticalSquareCellCount] =
+    React.useState(4)
   const [draggablePictures, setDraggablePictures] = React.useState(PICTURES)
   const [droppedPictures, setDroppedPictures] = React.useState(
     initialDestinationSquares
@@ -23,8 +30,66 @@ export default function VisualEditor() {
       id: `square-${droppedPictures.length + 1}`,
       picture: null,
     }
-
+    setVerticalSquareCellCount(verticalSquareCellCount + 1)
     setDroppedPictures([...droppedPictures, newSquare])
+  }
+
+  const handleSplitCell = (index, isHorizontal) => {
+    const updatedPictures = [...droppedPictures]
+    // Remove the original cell
+    const droppedPictureInfo = updatedPictures[index]
+    updatedPictures.splice(index, 1)
+
+    const createNewCell = (suffix) => ({
+      id: `${droppedPictureInfo.id}-${suffix}`,
+      picture: droppedPictureInfo.picture,
+      isSplit: true,
+      isHorizontalSplit: isHorizontal,
+      horizontalSplitCount: isHorizontal
+        ? droppedPictureInfo.horizontalSplitCount + 1
+        : droppedPictureInfo.horizontalSplitCount,
+      verticalSplitCount: isHorizontal
+        ? droppedPictureInfo.verticalSplitCount
+        : droppedPictureInfo.verticalSplitCount + 1,
+      widthPercent: 100,
+    })
+
+    const newCell1 = createNewCell(isHorizontal ? "horizontal" : "vertical")
+    const newCell2 = createNewCell(isHorizontal ? "horizontal" : "vertical")
+    // Insert the new cells at the same position as the original cell
+    updatedPictures.splice(index, 0, newCell1, newCell2)
+
+    // Update id for newCell2 and subsequent cells
+    const startIndex = updatedPictures.findIndex(
+      (cell) => cell.id === newCell2.id
+    )
+
+    for (let i = startIndex + 1; i < updatedPictures.length; i++) {
+      const [prefix, number, ...rest] = updatedPictures[i].id.split('-');
+    const newNumber = parseInt(number, 10) + 1;
+    updatedPictures[i].id = `${prefix}-${newNumber}${rest.length > 0 ? `-${rest.join('-')}` : ''}`;
+
+    }
+
+    //reset all width percentage to 100%
+    for (let i = 0; i < updatedPictures.length; i++) {
+      updatedPictures[i].widthPercent = 100;
+    }
+
+    //mark splitted pictures with 50% width percent
+    for (let i = 0; i < updatedPictures.length - 1; i++) {
+      const idLength = updatedPictures[i].id.split('-').length;
+      const nextIdLength = updatedPictures[i+1].id.split('-').length;
+
+      // Check if the current object and the next object have the same id length
+      if (idLength > 2 && (idLength === nextIdLength)) {
+        updatedPictures[i].widthPercent = 50;
+        updatedPictures[i + 1].widthPercent = 50;
+      }
+    }
+
+    console.log(updatedPictures)
+    setDroppedPictures(updatedPictures)
   }
 
   const handleDropEnd = (results) => {
@@ -41,6 +106,7 @@ export default function VisualEditor() {
       source.droppableId === 'source' &&
       destination.droppableId.startsWith('square')
     ) {
+      console.log('dragging from source to destination')
       const [removed] = updatedDraggablePictures.splice(source.index, 1)
       const destinationSquareIndex =
         parseInt(destination.droppableId.split('-')[1], 10) - 1
@@ -56,6 +122,7 @@ export default function VisualEditor() {
 
       setDraggablePictures(updatedDraggablePictures)
       setDroppedPictures(updatedDroppedPictures)
+      // console.log(droppedPictures)
     }
 
     // If dragging within the destination squares
@@ -63,6 +130,7 @@ export default function VisualEditor() {
       source.droppableId.startsWith('square') &&
       destination.droppableId.startsWith('square')
     ) {
+      console.log('dragging within destination')
       const sourceSquareIndex =
         parseInt(source.droppableId.split('-')[1], 10) - 1
       const destinationSquareIndex =
@@ -90,6 +158,7 @@ export default function VisualEditor() {
       source.droppableId.startsWith('square') &&
       destination.droppableId === 'source'
     ) {
+      console.log('dragging from destination back to source')
       const sourceIndex = parseInt(source.droppableId.split('-')[1], 10) - 1
 
       // Check if the square has a picture before attempting to remove it
@@ -143,6 +212,8 @@ export default function VisualEditor() {
               <LayoutArea
                 droppedPictures={droppedPictures}
                 onAddSquare={handleAddSquare}
+                verticalSquareCellCount={verticalSquareCellCount}
+                handleSplitCell={handleSplitCell}
               />
             </Card>
           </Grid.Col>
